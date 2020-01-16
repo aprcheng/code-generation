8eb1edf2b08d865d9c352f81af2abbed66641907
chengqiang <437004246@qq.com>
2020-01-16 10:17:34 +0800

    第一次提交

---
+++ src/main/java/com/chengqiang/code/generate/CodeGenerateApplication.java
@@ -0,0 +1,13 @@
+package com.chengqiang.code.generate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeGenerateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeGenerateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/chengqiang/code/generate/CodeGenerateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chengqiang.code.generate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeGenerateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
